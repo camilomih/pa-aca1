a07ad31ae9daee556fe317691e242e11b705e1e9
Camilo Valencia <camilomih@gmail.com@>
2022-12-01 12:03:21 -0500

    first commit

---
+++ ingresoEvento/ingresoEvento/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ingresoEvento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPresentacion = new System.Windows.Forms.Label();
+            this.txtbEdad = new System.Windows.Forms.TextBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPresentacion
+            // 
+            this.lblPresentacion.AutoSize = true;
+            this.lblPresentacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPresentacion.Location = new System.Drawing.Point(84, 46);
+            this.lblPresentacion.Name = "lblPresentacion";
+            this.lblPresentacion.Size = new System.Drawing.Size(156, 13);
+            this.lblPresentacion.TabIndex = 0;
+            this.lblPresentacion.Text = "Bienvenido al Evento XYZ";
+            this.lblPresentacion.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtbEdad
+            // 
+            this.txtbEdad.Location = new System.Drawing.Point(56, 88);
+            this.txtbEdad.Name = "txtbEdad";
+            this.txtbEdad.Size = new System.Drawing.Size(224, 20);
+            this.txtbEdad.TabIndex = 1;
+            this.txtbEdad.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(128, 126);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar.TabIndex = 2;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese su edad";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 203);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.txtbEdad);
+            this.Controls.Add(this.lblPresentacion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPresentacion;
+        private System.Windows.Forms.TextBox txtbEdad;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ingresoEvento/ingresoEvento/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ingresoEvento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Generar variables
+            int edad;
+            String edadTxt = txtbEdad.Text;
+
+            //Validar si es un numero
+            if (edadTxt.Trim() == "")
+            {
+                MessageBox.Show("Inserte un valor");
+                return;
+            }
+            
+            for(int i = 0; i < edadTxt.Length; i++)
+            {
+                if (!char.IsNumber(edadTxt[i]))
+                {
+                    MessageBox.Show("El valor solo debe ser numerico");
+                    return;
+                }
+            }
+
+            //Capturar variable
+            edad= int.Parse(txtbEdad.Text);
+
+            if(edad < 16)
+            {
+                MessageBox.Show("No puedes ingresar eres menor de edad");
+                return;
+            }
+
+            if(edad >= 16 && edad < 21)
+            {
+                MessageBox.Show("Puedes ingresar gratis, Bienvenido!");
+                return;
+            }
+
+            if (edad > 21)
+            {
+                MessageBox.Show("Bienvenido, debes pagar el ingreso.");
+                return;
+            }
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
